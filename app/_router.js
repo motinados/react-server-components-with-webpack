87ecb2d49c172853c6a2d2b87f7a70c51e872bd0
motinados <motinados@gmail.com>
2023-09-24 13:54:40 +0900

    createFromFetch

--- app/_router.js
+++ app/_router.js
@@ -1,13 +1,23 @@
 "use client";
+import { useState, use } from "react";
 import { createRoot } from "react-dom/client";
+import { createFromFetch } from "react-server-dom-webpack/client";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<Router />);
 
 function Router() {
-  return <ServerOutput />;
+  const [url, setUrl] = useState("rsc/" + "");
+  return <ServerOutput url={url} />;
 }
 
-function ServerOutput() {
-  return <div>ServerOutput</div>;
+const initialCache = new Map();
+
+function ServerOutput({ url }) {
+  const [cache, setCache] = useState(initialCache);
+  if (!cache.has(url)) {
+    cache.set(url, createFromFetch(fetch(url)));
+  }
+  const content = cache.get(url);
+  return use(content);
 }
