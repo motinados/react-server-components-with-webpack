58b59aae889ba123bcd6a1192ff35de38fc82ced
motinados <motinados@gmail.com>
2023-09-24 14:05:57 +0900

    add Albums

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,26 @@
+import { getAll } from "./db/get";
+
 export default function ServerRoot() {
   return (
     <div>
       <h1>Server Root</h1>
+      <Albums />
+    </div>
+  );
+}
+
+async function Albums() {
+  const albums = await getAll();
+  return (
+    <div>
+      <ul>
+        {albums.map((album) => (
+          <li key={album.id}>
+            <div>artist: {album.artist}</div>
+            <div>title: {album.title}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
